8bf54059fc5516e6923b9c0b657a6369d3ecd56e
mazpie <pietromazzaglia@gmail.com>
2023-02-02 17:31:22 +0000

    flip obs

--- gpulab/deploy_multi.sh
+++ gpulab/deploy_multi.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export GPUS=1
-export CPUS=2
-export MEMORY=8000
-export CLUSTER_ID=9
+export CPUS=8
+export MEMORY=64000
+export CLUSTER_ID=4
+export MOUNT_POINT="/project_ghent"
 export DOCKER_IMAGE="gitlab+deploy-token-8:iLvHxdZa4tyG4U5JXaCN@gitlab.ilabt.imec.be:4567/dml/gpulab/base:robosuite"
 
 export DML_DEPLOY_TOKEN="gitlab+deploy-token-461"
@@ -17,7 +18,7 @@ export DOMAIN="panda"
 export COMMAND="bash -c \\\" \
         wandb login $WANDB_KEY && \
         git config --global --add safe.directory /project_ghent/sferraro/choreographer && \
-        cd /project_ghent/sferraro/choreographer && \
+        cd  /project_ghent/sferraro/choreographer && \
         git pull origin robosuite && \
         python3 dreamer_pretrain.py configs=$CONFIG agent=$AGENT domain=$DOMAIN \
         \\\""
