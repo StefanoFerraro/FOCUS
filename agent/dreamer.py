002f2af5cc0ce40ae599c7fd6d8fcba4681f5326
mazpie <pietromazzaglia@gmail.com>
2023-04-03 08:50:26 +0000

    reward_fn UP

--- agent/dreamer.py
+++ agent/dreamer.py
@@ -97,9 +97,9 @@ class DreamerAgent(Module):
             lambda seq: self.wm.heads["object_decoder"](
                 seq["feat"], only_mlp=True
             )["objects_pos"][0]
-            .mean[:, :, 1]
+            .mean[:, :, 2]
             .unsqueeze(-1)
-        )  # move cubeA to the right (positive position)
+        )  # move cubeA Up (positive position)
 
         metrics.update(
             self._task_behavior.update(
