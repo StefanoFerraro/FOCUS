b55a49f5663892d1e5575095376811b6ba7214da
Toolkit User <stefano.ferraro@ugent.be>
2024-02-07 14:37:28 +0000

    init: code state before refactoring, after ICRA deadline

---
+++ agent/skill_dreamer.py
@@ -0,0 +1,107 @@
+import torch.nn as nn
+import torch
+from torch.nn.functional import normalize
+
+import utils
+import agent.dreamer_utils as common
+from collections import OrderedDict
+import numpy as np
+
+from agent.dreamer import DreamerAgent
+from agent.skill_utils import SkillActorCritic
+
+def stop_gradient(x):
+    return x.detach()
+
+Module = nn.Module
+
+class SkillDreamerAgent(DreamerAgent):
+    def __init__(self, name, cfg, obs_space, act_spec, **kwargs):
+        super().__init__(name, cfg, obs_space, act_spec, **kwargs)
+
+        self._mode = "train"
+        
+        # NOTE: Only for debugging
+        self._skill_strategy = 'task'
+        self._target_skill = self._target_pos[0][0]
+        self.skill_dim = [self._target_skill.shape[-1]]
+
+        self._skill_behavior = SkillActorCritic(cfg, self.act_spec, self.tfstep, skill_dim=self.skill_dim, 
+                                                    sampling_strategy=self._skill_strategy, solved_meta={'skill' : self._target_skill}) 
+
+        self.to(cfg.device)
+        self.requires_grad_(requires_grad=False)
+    
+    def task_reward_fn(self, seq):
+        rw = self.wm.heads["reward"](seq["feat"]).mean
+        return rw
+    
+    def update(self, data, step, which_policy='expl'):
+        
+        if which_policy not in ['expl', 'task', 'both']:
+            raise ValueError(f"which_policy must be one of ['expl', 'task', 'both'], got {which_policy}")
+        
+        dist =torch.exp(- torch.linalg.norm(data["objects_pos"] - self._target_pos, axis=-1) / torch.linalg.norm(self._target_pos))
+        data["reward"] = dist # substitute reward signal from the environment with the distance fromt the asigned target (conditioned on) and the currrent position
+        state, outputs, metrics = self.update_wm(data, step)
+
+        if step >= self.cfg.agent.start_agent_training_after:
+            if step == self.cfg.agent.start_agent_training_after:
+                print("STARTING AGENT TRAINING!")
+                
+            start = outputs["post"]
+            start = {k: stop_gradient(v) for k, v in start.items()}
+
+            self._target_skill = self._target_pos[0][0]
+            self._skill_behavior.solved_meta['skill'] = self._target_skill
+            
+            # agent update based on the achievement on the given skill 
+            if which_policy == 'expl':
+                NotImplementedError
+                
+            elif which_policy == 'task':
+                metrics.update(
+                    self._skill_behavior.update(
+                        self.wm, start, data["is_terminal"], getattr(self, f'{self._skill_strategy}_reward_fn')
+                    )
+                )
+            else:
+                metrics.update(
+                    self._skill_behavior.update(
+                        self.wm, start, data["is_terminal"], getattr(self, f'{self._skill_strategy}_reward_fn')
+                    )
+                )
+            
+        return state, metrics
+
+    def act(self, obs, meta, step, eval_mode, state):
+        obs = {
+            k: torch.as_tensor(np.copy(v), device=self.device).unsqueeze(0)
+            for k, v in obs.items()
+        }
+        if state is None:
+            latent = self.wm.rssm.initial(len(obs["reward"]))
+            action = torch.zeros(
+                (len(obs["reward"]),) + self.act_spec.shape, device=self.device
+            )
+        else:
+            latent, action = state
+        embed = self.wm.encoder(self.wm.preprocess(obs))
+        should_sample = (not eval_mode) or (not self.cfg.eval_state_mean)
+        latent, _ = self.wm.rssm.obs_step(
+            latent, action, embed, obs["is_first"], should_sample
+        )
+        feat = self.wm.rssm.get_feat(latent)
+
+        policy =  self._skill_behavior.actor
+        skill = self._target_pos[0][0]
+        inp = torch.cat([feat, skill], dim=-1)
+
+        actor = policy(inp)
+        if eval_mode:
+            action = actor.mean
+        else:
+            action = actor.sample()
+
+        new_state = (latent, action)
+        return action.cpu().numpy()[0], new_state
---
+++ trainval_offline_train.py
@@ -0,0 +1,91 @@
+import sys
+from haven import haven_wizard as hw
+import hydra
+import os
+from offline_pretrain import toolkit_main
+
+import argparse
+import exp_configs
+import job_configs
+from pathlib import Path
+from omegaconf import DictConfig, OmegaConf, open_dict
+
+@hydra.main(config_path='configs', config_name='dreamer_pretrain')
+def get_config(cfg):
+    global config
+    config = cfg
+
+def trainval(exp_dict, savedir, args):
+    """
+    exp_dict: dictionary defining the hyperparameters of the experiment
+    savedir: the directory where the experiment will be saved
+    args: arguments passed through the command line
+    """
+    config_dict = {k.replace('|', '.') : v for k,v in exp_dict.items()}
+    wandb_dict = {k: v for k,v in exp_dict.items() if "|" in k}
+    
+    # unpack config elements to be checkable from wandb
+    sys.argv=["offline_pretrain.py"] + [ f"{k}={v}" for k,v in config_dict.items()] 
+    get_config()
+    
+    wandb_conf = OmegaConf.create(wandb_dict)
+    with open_dict(config):
+        config.merge_with(wandb_conf)
+        
+    # original_cwd = hydra.utils.get_original_cwd()
+    config.comment = args.project_name
+    # cfg.agent = Bunch(cfg.agent)
+    config.snapshot_dir = f"/mnt/public/projects/{args.user}/{args.project_name}/pretrained_models/{config_dict['agent']}/{config_dict['env']}/{config_dict['task']}/{config_dict['seed']}"
+    
+    toolkit_main(config, maindir=savedir, workdir=Path.cwd())
+
+    print("Experiment completed")
+
+def _init_wandb(user):
+    os.environ["WANDB_API_KEY"] = job_configs.WANDB_API_KEY[user]
+    os.environ["WANDB_DIR"] = "/tmp/"
+    os.environ["WANDB_DISABLE_CODE"] = "true"
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-sb",
+        "--savedir_base",
+        default="",
+        type=str,
+        help="Define the base directory where the experiments will be saved.",
+    )
+    parser.add_argument(
+        "-r", "--reset", default=0, type=int, help="Reset or resume the experiment."
+    )
+    parser.add_argument("-c", "--cuda", default=1, type=int)
+    parser.add_argument("-j", "--job_scheduler", default=None, type=str)
+    parser.add_argument("-nw", "--num_workers", default=0, type=int)
+    parser.add_argument("-u", "--user", type=str, required=True)
+    parser.add_argument("-p", "--project_name", type=str, required=True)
+    args, others = parser.parse_known_args()
+    _init_wandb(args.user)
+
+    if args.job_scheduler == "slurm":
+        job_config = {
+            "account_id": "rrg-bengioy-ad",
+            "time": "12:00:00",
+            "cpus-per-task": "2",
+            "mem-per-cpu": "16G",
+            "gres": "gpu:1",
+        }
+    elif args.job_scheduler == "toolkit":
+        job_config = job_configs.JOB_CONFIG
+    else:
+        job_config = None
+
+    hw.run_wizard(
+        func=trainval,
+        exp_list=exp_configs.EXP_GROUPS[args.project_name+"_pretrain"],
+        job_config=job_configs.JOB_CONFIG[args.user],
+        python_binary_path=job_configs.PYTHON_BINARIES[args.user],
+        savedir_base=args.savedir_base,
+        use_threads=True,
+        args=args,
+    )
